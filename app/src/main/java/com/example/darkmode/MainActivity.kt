ab71a596d6ec3baf9a9ed20600fd108a59fdbbec
Frost1801 <sergio.cibecchini@gmail.com>
2024-01-08 21:49:29 +0100

    Added a fun easter egg in the app

--- app/src/main/java/com/example/darkmode/MainActivity.kt
+++ app/src/main/java/com/example/darkmode/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.darkmode
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.Switch
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.appcompat.widget.SwitchCompat
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
-import com.example.darkmode.ui.theme.DarkModeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +15,41 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.mainlayout)
         val toggleButton = findViewById<SwitchCompat>(R.id.material_switch)
         val mainLayout = findViewById<LinearLayoutCompat> (R.id.mainLinearLayout)
+        val monsterImage = findViewById<ImageView>(R.id.monster)
+        val textBox = findViewById<TextView> (R.id.mainTextBox)
+
+        var clicks = 0
 
         toggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            clicks ++
             if (isChecked){
+                if (clicks == 1){
+                    textBox.text = ContextCompat.getString(this,R.string.reassuring)
+
+                    val descriptionBox = findViewById<TextView>(R.id.descriptionbox)
+                    descriptionBox.visibility = View.GONE
+                }
+                else if (clicks > 3){
+                    textBox.text = ContextCompat.getString(this, R.string.run)
+                }
+                else{
+                    textBox.text = ContextCompat.getString(this,R.string.cantEscape)
+                }
                 mainLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.black))
+                toggleButton.setTextColor(ContextCompat.getColor(this, R.color.white))
+                textBox.setTextColor(ContextCompat.getColor(this,R.color.white))
             }
             else{
+                if (clicks == 2){
+                    textBox.text = ContextCompat.getString(this,R.string.memeText)
+                    monsterImage.visibility =  View.VISIBLE
+                }
+                else if (clicks == 3){
+                    textBox.text = ContextCompat.getString(this,R.string.cantEscape)
+                }
+                textBox.setTextColor(ContextCompat.getColor(this,R.color.black))
                 mainLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.white))
+                toggleButton.setTextColor(ContextCompat.getColor(this, R.color.black))
             }
         }
 
