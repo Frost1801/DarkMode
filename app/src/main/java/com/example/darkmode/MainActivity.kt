37865f97c55a08d1d6726896f95b0c5d9621076f
Frost1801 <sergio.cibecchini@gmail.com>
2024-01-08 22:12:40 +0100

    Improved app functionality

--- app/src/main/java/com/example/darkmode/MainActivity.kt
+++ app/src/main/java/com/example/darkmode/MainActivity.kt
@@ -10,43 +10,32 @@ import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
+    private lateinit var toggleButton : SwitchCompat
+    private lateinit var mainLayout  : LinearLayoutCompat
+    private lateinit var  monsterImage : ImageView
+    private lateinit var textBox : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainlayout)
-        val toggleButton = findViewById<SwitchCompat>(R.id.material_switch)
-        val mainLayout = findViewById<LinearLayoutCompat> (R.id.mainLinearLayout)
-        val monsterImage = findViewById<ImageView>(R.id.monster)
-        val textBox = findViewById<TextView> (R.id.mainTextBox)
+
 
         var clicks = 0
+        toggleButton = findViewById<SwitchCompat>(R.id.material_switch)
+        mainLayout = findViewById<LinearLayoutCompat> (R.id.mainLinearLayout)
+        monsterImage = findViewById<ImageView>(R.id.monster)
+        textBox = findViewById<TextView> (R.id.mainTextBox)
 
         toggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
             clicks ++
+            switchWasClicked(clicks)
             if (isChecked){
-                if (clicks == 1){
-                    textBox.text = ContextCompat.getString(this,R.string.reassuring)
-
-                    val descriptionBox = findViewById<TextView>(R.id.descriptionbox)
-                    descriptionBox.visibility = View.GONE
-                }
-                else if (clicks > 3){
-                    textBox.text = ContextCompat.getString(this, R.string.run)
-                }
-                else{
-                    textBox.text = ContextCompat.getString(this,R.string.cantEscape)
-                }
                 mainLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.black))
                 toggleButton.setTextColor(ContextCompat.getColor(this, R.color.white))
                 textBox.setTextColor(ContextCompat.getColor(this,R.color.white))
             }
             else{
-                if (clicks == 2){
-                    textBox.text = ContextCompat.getString(this,R.string.memeText)
-                    monsterImage.visibility =  View.VISIBLE
-                }
-                else if (clicks == 3){
-                    textBox.text = ContextCompat.getString(this,R.string.cantEscape)
-                }
+
                 textBox.setTextColor(ContextCompat.getColor(this,R.color.black))
                 mainLayout.setBackgroundColor(ContextCompat.getColor(this,R.color.white))
                 toggleButton.setTextColor(ContextCompat.getColor(this, R.color.black))
@@ -54,6 +43,36 @@ class MainActivity : ComponentActivity() {
         }
 
 
+
     }
+    private fun switchWasClicked (clicks: Int){
+        when (clicks){
+            1 -> {
+                textBox.text = ContextCompat.getString(this,R.string.reassuring)
+                val descriptionBox = findViewById<TextView>(R.id.descriptionbox)
+                descriptionBox.visibility = View.GONE
+            }
+            2->{
+                textBox.text = ContextCompat.getString(this,R.string.insideWalls)
+                monsterImage.visibility =  View.VISIBLE
+            }
+            3-> {
+                textBox.text = ContextCompat.getString(this,R.string.cantEscape)
+            }
+            4-> {
+                textBox.text = ContextCompat.getString(this,R.string.cantEscape)
+            }
+            5 -> {
+                textBox.text = ContextCompat.getString(this, R.string.run)
+            }
+            6 -> {
+                monsterImage.setImageResource(R.drawable.monsterwithhands)
+
+            }
+        }
+
+    }
+
+
 }
 
